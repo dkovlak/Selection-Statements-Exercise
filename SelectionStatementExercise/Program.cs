75226f8ff96fe2d8411fb6a7512ebfe321e1c784
David Kovlak <kovlakdavid4@gmail.com>
2023-08-29 09:08:46 -0700

    Guess

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(10);
+
+            Console.WriteLine("Gues the number:");
+
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (favNumber > userInput)
+            {
+                Console.WriteLine("too low");
+            }
+            else if (favNumber < userInput)
+            {
+                Console.WriteLine("too high");
+            }
+            else
+            {
+                Console.WriteLine("You guesse it!!!");
+            }
         }
     }
 }
